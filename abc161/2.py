bff8cbf3cefe2d786c0c9f0398c7f2bdd42e0821
TaxiN <nthouse14@gmail.com>
2020-04-05 09:10:46 +0900

    modify 2.py

--- abc161/2.py
+++ abc161/2.py
@@ -1,11 +1,13 @@
 N, M = map(int, input().split())
 list_goods = [int(x) for x in input().split()]
 
+total = sum(list_goods)
 list_goods.sort(reverse=True)
 
 flag = "Yes"
 
-if list_goods[M-1] < sum(list_goods)//(4*M):
+# 割り切れないケースも考慮して / で計算
+if list_goods[M-1] < total/(4*M):
     flag = "No"
 
 print(flag)
